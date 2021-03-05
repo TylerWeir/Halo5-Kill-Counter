68c0e3d4f9e303f53221d33b55331baaea2eebb7
TylerWeir <tylerw@live.com>
2021-03-04 23:42:50 -0800

    Displays kills deaths and kd for each game.

--- halo5_kills.py
+++ halo5_kills.py
@@ -2,7 +2,7 @@ import requests
 
 
 # Make an API call and store the response
-url = 'https://www.haloapi.com/stats/h5/players/EnduroCat14/matches'
+url = 'https://www.haloapi.com/stats/h5/players/MrFlyhigh/matches'
 headers = {'Ocp-Apim-Subscription-Key': 'f83454ddd4fb4e4da43f9752693bf8e2'}
 r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
@@ -17,5 +17,7 @@ for i in range(len(game_dicts)):
     game_dict = game_dicts[i]   # Examine the ith game
     player_dict = game_dict['Players']
     endurocat = player_dict[0]
-    print(f"Game {i+1}: \t{endurocat['TotalKills']}")        
+    print(f"Game {i+1}: \tKills: {endurocat['TotalKills']} 
+                        \tDeaths: {endurocat['TotalDeaths']} 
+                        \tKD: {endurocat['TotalKills']/endurocat['TotalDeaths']}")        
 
